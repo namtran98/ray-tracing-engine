969ebcccc8bfd0ef819fa975b77e50ab03ef5e39
Owen Gillespie <owenlgillespie@gmail.com>
2019-04-27 16:20:23 -0700

    Clean up vector

--- raytracer/utilities/Vector3D.cpp
+++ raytracer/utilities/Vector3D.cpp
@@ -1,12 +1,12 @@
 #include "Vector3D.hpp"
 #include "Point3D.hpp"
-#include <math.h> 
+#include <math.h>
 
-Vector3D::Vector3D() 
+Vector3D::Vector3D()
     : x(0), y(0), z(0) {
 }
 
-Vector3D::Vector3D(double c) 
+Vector3D::Vector3D(double c)
     : x(c), y(c), z(c) {
 }
 
@@ -14,24 +14,20 @@ Vector3D::Vector3D(double _x, double _y, double _z)
     : x(_x), y(_y), z(_z) {
 }
 
-Vector3D::Vector3D(const Point3D& p) 
-    : x(p.x), y(p.y), z(p.z) {
-}
+Vector3D::Vector3D(const Point3D& p)
+    : x(p.x), y(p.y), z(p.z) {}
 
-Vector3D::Vector3D(const Vector3D& v) {
-    x = v.x;
-    y = v.y;
-    z = v.z;
-}
+Vector3D::Vector3D(const Vector3D& v)
+    : x(v.x), y(v.y), z(v.z) {}
 
 Vector3D::~Vector3D() {
 
 }
 
 Vector3D& Vector3D::operator= (const Vector3D& rhs) {
-    this -> x = rhs.x;
-    this -> y = rhs.y;
-    this -> z = rhs.z;
+    x = rhs.x;
+    y = rhs.y;
+    z = rhs.z;
     return *this;
 }
 
