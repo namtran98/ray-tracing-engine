fad0411388e19600337308a34d7a8261990a038b
Owen Gillespie <owenlgillespie@gmail.com>
2019-04-27 16:20:13 -0700

    Implement bounding box

---
+++ raytracer/acceleration/BBox.hpp
@@ -0,0 +1,14 @@
+#include "../utilities/Ray.hpp"
+
+class BBox{
+public:
+  BBox();
+  BBox(Point3D min, Point3D max);
+  ~BBox();
+  BBox(const BBox& v);
+  BBox& operator= (const BBox& rhs);
+
+  bool hit(const Ray& ray) const;
+  Point3D min_point; // (x0, y0, z0)
+  Point3D max_point; // (x1, y1, z1)
+};
